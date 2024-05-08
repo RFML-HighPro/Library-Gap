156e601d2f93c2fcef7b761471c862629908d487
RFML-HighPro <ryanfelipemaiamelo@gmail.com>
2024-05-08 15:57:35 -0300

    chore: adjustment of dtos(User and Book)

--- src/main/java/br/com/livraygap/library/dtos/BookDTO.java
+++ src/main/java/br/com/livraygap/library/dtos/BookDTO.java
@@ -1,22 +1,42 @@
 package br.com.livraygap.library.dtos;
 
-import br.com.livraygap.library.entities.User;
-
-import java.math.BigInteger;
-import java.util.Date;
+import br.com.livraygap.library.entities.Book;
+import br.com.livraygap.library.enums.GenreEnum;
 
 public record BookDTO(
+        Long id,
         String title,
         String author,
         String publisher,
-        int pages,
-        User.EnumTypeUser genre,
-        BigInteger isbn,
+        Integer pages,
+        GenreEnum genre,
+        String isbn,
         Double price,
-        Date release_date,
-        Date created_at,
-        Date updated_at,
-        Boolean deleted_at
+        String release_date,
+        String created_at,
+        Boolean deleted_at,
+        String updated_at,
+        Integer stocked,
+        Double rating,
+        Integer sales
 ) {
+    public static BookDTO getDtoBook(Book book){
+        return new BookDTO(
+                book.getId(),
+                book.getTitle(),
+                book.getAuthor(),
+                book.getPublisher(),
+                book.getPages(),
+                book.getGenre(),
+                book.getIsbn(),
+                book.getPrice(),
+                book.getReleaseDate(),
+                book.getCreatedAt(),
+                book.getDeletedAt(),
+                book.getUpdatedAt(),
+                book.getStocked(),
+                book.getRating(),
+                book.getSales()
+        );
+    }
 }
-
--- src/main/java/br/com/livraygap/library/dtos/UserDTO.java
+++ src/main/java/br/com/livraygap/library/dtos/UserDTO.java
@@ -1,13 +1,29 @@
 package br.com.livraygap.library.dtos;
 
 import br.com.livraygap.library.entities.User;
+import br.com.livraygap.library.enums.UserEnum;
 
-public record UserDTO (
+public record UserDTO(
         Long id,
         String name,
         String lastname,
         String contact,
         String email,
         String cpf,
-        User.EnumTypeUser type_user) {
+        String created_at,
+        String updated_at,
+        UserEnum typeUser) {
+    public static UserDTO getDtoUser(User user) {
+        return new UserDTO(
+                user.getId(),
+                user.getName(),
+                user.getLastname(),
+                user.getContact(),
+                user.getEmail(),
+                user.getCpf(),
+                user.getCreatedAt(),
+                user.getUpdatedAt(),
+                user.getTypeUser()
+        );
+    }
 }
